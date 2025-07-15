49db891e2dcf26c7428687515aa11d02d855b7de
Kieron <kieron.hushon@gmail.com>
2025-07-15 13:38:05 +0100

    Format code with black

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,12 +3,13 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.main import app  # This should now work
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
+
 
 def test_predict_endpoint():
     sample_input = {
@@ -30,7 +31,7 @@ def test_predict_endpoint():
         "PaperlessBilling": "Yes",
         "PaymentMethod": "Electronic check",
         "MonthlyCharges": 70.35,
-        "TotalCharges": 350.5
+        "TotalCharges": 350.5,
     }
 
     response = client.post("/predict", json=sample_input)
